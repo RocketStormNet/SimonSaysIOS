f4e7e588ce24cd4e5ee95443baa4ad2b96df529e
RocketStormNet <rocketstormnet@outlook.com>
2018-11-16 11:13:28 +0500

    Initial Commit

---
+++ Simon Says/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Mikhail Timofeev on 16/11/2018.
+//  Copyright © 2018 rocketstorm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
